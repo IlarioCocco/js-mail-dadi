61906059b5b5ea64d2eb255a4070cd0685736e78
IlarioCOCCO <ilario.cocco@gmail.com>
2021-08-22 23:44:35 +0200

    gioco Dadi

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,28 @@
-alert("ciao");
+// Gioco dei dadi
+
+// 1 Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+var dadoGiocatore = (Math.floor(Math.random() * 6) + 1);
+console.log("Dado Giocatore:" + dadoGiocatore)
+alert(dadoGiocatore)
+
+var dadoComputer = (Math.floor(Math.random() * 6) + 1);
+console.log("Dado Computer:" + dadoComputer)
+alert(dadoComputer)
+
+
+// 2 Stabilire il vincitore, in base a chi fa il punteggio più alto.
+if (dadoGiocatore > dadoComputer) {
+    alert("Result : " + "hai vinto!");
+    console.log("Result : " + "hai vinto!");
+
+} else if (dadoGiocatore < dadoComputer) {
+    alert("Result : " + "hai perso! Pc win!");
+    console.log("Result : " + "hai perso! Pc win!");
+
+} else {
+    alert("Pareggio");
+    console.log("Result : " + "Pareggio!");
+}
+// Prima di partire a scrivere codice poniamoci qualche domanda:
+// Che ci sia un array da qualche parte ?
+// Se dobbiamo confrontare qualcosa che "cosa" ci serve ?
